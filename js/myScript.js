e4a4bf2283892053351609eefbffea305414a15a
zone <43332748+plateau11@users.noreply.github.com>
2023-03-23 13:49:46 +0530

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -3,6 +3,7 @@ var q21 = "\"If you want to lift yourself up, lift up someone else\" - Booker T.
 var q22 = "\"You’re braver than you believe, stronger than you seem, and smarter than you think.\" - A.A. Milne";
 var q23 = "\"It took me quite a long time to develop a voice, and now that I have it, I am not going to be silent.\" - Madeleine Albright";
 var q24 = "\"There is no misery that could be a match for My mercy, neither will misery exhaust it, because as it is being granted - it increases.\" - Divine Mercy Jesus";
+var q25 = "\" Don't solve your problems, dissolve it. \" - Anonymous";
 
 var element = document.querySelector(".quotes");
 
@@ -26,6 +27,9 @@ else if(day == 23)
 
 else if(day == 24) 
   element.textContent = q + q24;
+
+else if(day == 25) 
+  element.textContent = q + q25;
 
 else
   element.textContent = q;
